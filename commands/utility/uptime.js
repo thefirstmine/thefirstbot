0312be4a9d4ab8f6ac407d5ad39c14d2148f0112
TheFirstMine <tfmphbusiness@gmail.com>
2021-07-15 18:03:52 +0800

    Remove day counter from uptime command

--- commands/utility/uptime.js
+++ commands/utility/uptime.js
@@ -6,11 +6,10 @@ module.exports = {
         category: "Utility",
 	execute(client, message, args) {
         let totalSeconds = (client.uptime / 1000);
-        let days = Math.floor(totalSeconds / 86400);
         let hours = Math.floor(totalSeconds / 3600);
         totalSeconds %= 3600;
         let minutes = Math.floor(totalSeconds / 60);
-        let uptime = `${days} days, ${hours} hours, and ${minutes} minutes`;
+        let uptime = `${hours} hours, and ${minutes} minutes`;
 
         const uptimeEmbed = new Discord.MessageEmbed()
         .setTitle("<:uptime:859760884007436309> Bot uptime")
