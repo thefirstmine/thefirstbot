a8d90d4c2096820ced832500abc645155d793155
TheFirstMine <tfmphbusiness@gmail.com>
2021-07-22 17:31:15 +0800

    Added stats command, removed uptime and ping to merge to stats command, added moemnt-duration-format as a dependency

---
+++ commands/miscellaneous/stats.js
@@ -0,0 +1,50 @@
+const { MessageEmbed } = require("discord.js");
+require("moment-duration-format");
+const cpuStat = require("cpu-stat");
+const moment = require("moment");
+
+module.exports = {
+	name: 'stats',
+	description: 'Check bot stats!',
+    category: "Miscellaneous",
+    aliases: ['statistics', 'uptime', 'ping'],
+	execute(client, message, args) {
+        const { version } = require("discord.js")
+        cpuStat.usagePercent(async function (err, percent, seconds) {
+        if (err) {
+            return console.log(err);
+        }
+        const duration = moment.duration(message.client.uptime).format(" D[d], H[h], m[m]");
+
+        const embed = new MessageEmbed()
+        embed.setColor("#FCBA03")
+        embed.setTitle(`Stats for ${client.user.username}!`)
+        embed.addFields({
+            name: ':ping_pong: Ping',
+            value: `┕\`${Math.round(client.ws.ping)}ms\``,
+            inline: true
+        },
+        {
+            name: ':clock1: Uptime',
+            value: `┕\`${duration}\``,
+            inline: true
+        },{
+            name: ':file_cabinet: Memory',
+            value: `┕\`${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}mb\``,
+            inline: true
+        })
+
+        embed.addFields({
+            name: ':blue_book: Discord.js', 
+            value: `┕\`v${version}\``,
+            inline: true
+        },{
+            name: ':green_book: Node',
+            value: `┕\`${process.version}\``,
+            inline: true
+        })
+
+        return message.channel.send(embed);
+        })
+	},
+};
--- commands/utility/ping.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	name: 'ping',
-	description: 'Ping! Checks ping, duh',
-	category: "Utility",
-	execute(client, message, args) {
-        message.channel.send(`🏓 Pong! ${Date.now() - message.createdTimestamp}ms.`)
-	},
-};
