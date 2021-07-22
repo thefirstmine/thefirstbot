36a8b5377205d44e2af166f8c5619ead3fc3eb58
TheFirstMine <tfmphbusiness@gmail.com>
2021-07-22 18:04:37 +0800

    Add avatar command

---
+++ commands/utility/avatar.js
@@ -0,0 +1,31 @@
+const Discord = require("discord.js")
+
+module.exports = {
+	name: 'avatar',
+	description: 'Get someones profile picture!',
+    category: "Utility",
+    aliases: ['pfp'],
+	execute(client, message, args) {
+        if(!args.length){
+            target = message.guild.member(message.author)
+        } else if (message.mentions.users.size) {
+            target = message.mentions.members.first();
+        } else if (args[0].match(/^([0-9]{15,21})$/)) {
+            target = message.guild.members.cache.get(args[0]);
+        } else {
+            target = message.guild.members.cache.find(x => x.user.username.toLowerCase() === args.join(' ').toLowerCase() ||
+                x.user.tag.toLowerCase() === args.join(' ').toLowerCase());
+        }
+        if (!target) return message.reply("invalid user.")
+
+        const avatarEmbed = new Discord.MessageEmbed()
+        .setTitle(`${target.user.username}\'s avatar!`)
+        .setDescription(
+        `[Default link](${target.user.avatarURL({dynamic: true, size: 1024})})\nOther image formats:\n[.webp](${target.user.avatarURL({format: "webp", size: 1024})}) | [.png](${target.user.avatarURL({format: "png", size: 1024})}) | [.jpg](${target.user.avatarURL({format: "jpg", size: 1024})})`
+        )
+        .setImage(target.user.avatarURL({dynamic: true, size: 1024}))
+        .setColor("#FCBA03")
+
+        message.channel.send(avatarEmbed)
+	},
+};
